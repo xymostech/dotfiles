71021bb6cf702104fefb00144b688faff7919b42
Emily Eisenberg <xymostech@gmail.com>
2014-07-17 22:33:51 -0600

    Make the t command better

--- fish/functions/t.fish
+++ fish/functions/t.fish
@@ -1,7 +1,9 @@
 function t
     if tmux -q has-session -t $argv 2>/dev/null
         command tmux -2 attach-session -d -t $argv
-    else
+    else if tmuxifier ls | grep -q $argv
         command tmuxifier s $argv
+    else
+        command tmux -2 new-session -s $argv
     end
 end
