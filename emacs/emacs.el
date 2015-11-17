f6ce48f545b610651d3f3600c31e5e2445f04f74
Emily Eisenberg <xymostech@gmail.com>
2015-11-16 18:24:11 -0800

    Update emacs config

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -9,8 +9,8 @@
 (menu-bar-mode -1)
 
 ; Set some default tab settings
-(setq-default c-basic-offset 4 ; indent amount
-              tab-width 4 ; size of tabs
+(setq-default c-basic-offset 4      ; indent amount
+              tab-width 4           ; size of tabs
               indent-tabs-mode nil) ; don't use tab characters
 
 ; Use linux-style bracketing
@@ -28,16 +28,22 @@
       '((".*" "~/.tmp" t)))
 
 ; Save the place I last visited files
-(require 'saveplace)
-(setq-default save-place t)
-(setq save-place-file "~/.emacs.d/save-places")
+;(require 'saveplace)
+;(setq-default save-place t)
+;(setq save-place-file "~/.emacs.d/save-places")
+
+; Turn off the blinking cursor
+(setq blink-cursor-mode nil)
+
+; Highlight the line the cursor is on
+(global-hl-line-mode t)
 
 ; Set the default line wrap
 (setq-default fill-column 79)
 
 ; Use the x clipboard
-(setq x-select-enable-primary nil)
-(setq x-select-enable-clipboard t)
+;(setq x-select-enable-primary nil)
+;(setq x-select-enable-clipboard t)
 
 ; Make the mark ring better
 (setq set-mark-command-repeat-pop t)
@@ -47,30 +53,30 @@
 (setq sentence-end-double-space nil)
 
 ; Make scrolling never jump, ever
-(setq scroll-margin 3)
-(setq scroll-conservatively 10000)
-(setq scroll-up-aggressively 0.01)
-(setq scroll-down-aggressively 0.01)
-(setq scroll-step 1)
-(setq scroll-preserve-screen-position 1)
-(setq redisplay-dont-pause t)
+;(setq scroll-margin 3)
+;(setq scroll-conservatively 10000)
+;(setq scroll-up-aggressively 0.01)
+;(setq scroll-down-aggressively 0.01)
+;(setq scroll-step 1)
+;(setq scroll-preserve-screen-position 1)
+;(setq redisplay-dont-pause t)
 
 ; Make unique filenames more useful
-(require 'uniquify)
-(setq uniquify-buffer-name-style 'reverse)
-(setq uniquify-after-kill-buffer-p t)
-(setq uniquify-ignore-buffers-re "^\\*")
+;(require 'uniquify)
+;(setq uniquify-buffer-name-style 'reverse)
+;(setq uniquify-after-kill-buffer-p t)
+;(setq uniquify-ignore-buffers-re "^\\*")
 
 ; Setup ido
 (require 'ido)
 (ido-mode t)
-(ido-everywhere t)
-(setq ido-enable-flex-matching t)
-(setq ido-use-filename-at-point nil)
-(setq ido-create-new-buffer 'always)
+;(ido-everywhere t)
+;(setq ido-enable-flex-matching t)
+;(setq ido-use-filename-at-point nil)
+;(setq ido-create-new-buffer 'always)
 
 ; Always indent after return. I would try using C-j, but it conflicts with tmux
-(define-key global-map (kbd "RET") 'newline-and-indent)
+;(define-key global-map (kbd "RET") 'newline-and-indent)
 
 ; Turn on column numbering
 (setq column-number-mode t)
@@ -82,9 +88,16 @@
 
 ; The list of packages I install through package.el
 (defvar managed-packages
-  '(magit haskell-mode js2-mode nlinum less-css-mode
-          color-theme rust-mode smart-mode-line lua-mode
-          markdown-mode glsl-mode))
+  '(magit haskell-mode
+          ;js2-mode
+          less-css-mode
+          color-theme
+          ;rust-mode
+          smart-mode-line
+          ;lua-mode
+          markdown-mode
+          ;glsl-mode
+          ))
 
 ; The list of packages I manually manage
 (defvar manual-packages
@@ -94,7 +107,7 @@
 (require 'package)
 (package-initialize)
 
-; Ensure all of the packages
+; Ensure all of the packages are loaded
 ;(package-refresh-contents)
 ;(dolist (package managed-packages)
   ;(when (not (package-installed-p package))
@@ -114,12 +127,6 @@
 
 ; ------- Filetype Specific Settings -------
 
-(defvar mode-autoloads
-  '(
-    ('markdown-mode . "markdown-mode")
-    )
-  )
-
 (mapcar
  (lambda (data)
           (autoload (car data) (cdr data) nil t))
@@ -129,12 +136,11 @@
    (web-mode . "web-mode")
   ))
 
-(setq auto-mode-alist
-      (append auto-mode-alist
-              '(
-                ("\\.jsx\\'" . web-mode)
-                ("\\.cmake\\'" . cmake-mode)
-               )))
+; (setq auto-mode-alist
+;       (append auto-mode-alist
+;               '(
+;                 ("\\.jsx\\'" . web-mode)
+;                )))
 
 ; change some indenting settings in C++
 ;(add-hook 'c++mode-hook
@@ -155,7 +161,7 @@
 ;(autoload 'markdown-mode "markdown-mode" nil t)
 ;(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+(add-hook 'haskell-mode-hook 'haskell-indent-mode)
 
 ;(require 'lua-mode)
 ;(setq lua-indent-level 2)
@@ -165,23 +171,15 @@
 
 ; ------- Plugin Specifc Settings --------
 
-; Disable some evil features
-;(setq evil-want-C-i-jump nil)
-
-; Enable evil-mode globally
-;(require 'evil)
-;(evil-mode 1)
-
-; Some modes need emacs mode
-;(evil-set-initial-state 'git-rebase-mode 'emacs)
-
-; Setup nlinum
-(require 'nlinum)
-(setq nlinum-format "%d ")
-(global-nlinum-mode 1)
-
-; Enable my colorscheme
-(require 'molokai-theme)
+; Setup linum (cause nlinum causes segfaults!?)
+(require 'linum)
+; Use our own formatting function to get the number with a single
+; space after it.
+(defun linum-format-func (line)
+  (let ((w (length (number-to-string (count-lines (point-min) (point-max))))))
+    (propertize (format (format "%%%dd " w) line) 'face 'linum)))
+(setq linum-format 'linum-format-func)
+(global-linum-mode 1)
 
 ;(autoload 'rust-mode "rust-mode" nil t)
 ;(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
@@ -206,7 +204,16 @@
 
 ; Setup smart-mode-line
 (require 'smart-mode-line)
-(add-to-list 'sml/replacer-regexp-list '("^~/repos/dotfiles/" ":.files:") t)
+(setq
+ sml/replacer-regexp-list
+ '(
+   ("^~/repos/dotfiles/" ":dotfiles:")
+   ("^~/khan/webapp/" ":webapp:")
+   ("^~/Dropbox/Projects/" ":projects:")
+   )
+ )
+(setq sml/theme 'dark)
+;(add-to-list 'sml/replacer-regexp-list '("^~/repos/dotfiles/" ":.files:") t)
 (setq sml/no-confirm-load-theme t)
 (sml/setup)
 
@@ -219,8 +226,8 @@
 ;            (add-to-list 'fill-nobreak-predicate 'texmathp)))
 
 ; Enable some whespace settings
-(global-whitespace-mode 1)
 (setq whitespace-style '(face trailing spaces-mark tab-mark))
+(global-whitespace-mode 1)
 
 ; Setup less
 ;(autoload 'less-css-mode "less-css-mode" nil t)
@@ -229,20 +236,24 @@
 ; Setup editor config
 ;(load "editorconfig")
 
-(require 'magit)
+; Set up magit
+;(require 'magit)
 
 ; -------- Random Extra Functions -------
 
+; Paste from the x clipboard
 (defun x-paste ()
   (interactive)
   (call-process "xclip" nil t nil "-o" "-selection" "clipboard"))
 
+; Copy to the x clipboard
 (defun x-copy ()
   (interactive)
   (call-process-region (region-beginning) (region-end)
                        "xclip" nil 0 nil "-i" "-selection" "clipboard")
   (deactivate-mark))
 
+; Fix the javascript regexp regexp for js-mode
 ;(add-hook 'js-mode-hook
 ;  (lambda ()
 ;    (defun js-syntax-propertize-regexp (end)
@@ -251,3 +262,33 @@
 ;        (when (re-search-forward "\\(\\/\\|\\\[[^]]*?\\\]\\|[^/[]\\)*?\\(\\/\\|\\\[[^]]*\\]\\|[^\\/]\\)/" end 'move)
 ;          (put-text-property (1- (point)) (point)
 ;                             'syntax-table (string-to-syntax "\"/")))))))
+
+; Calculate the closest defined (i.e. builtin) color to a given
+; color. This is done automatically by emacs when given a color that
+; isn't supported, so isn't necessary, but is interesting to see
+; cl-lib in action.
+
+;(require 'cl-lib)
+;(defun closest-defined-color (color)
+;  (cl-reduce
+;   (lambda (prev-best test)
+;     (if (< (color-distance color test) (color-distance color prev-best))
+;         test
+;       prev-best))
+;   (defined-colors)
+;   :initial-value "black"))
+
+(custom-set-faces
+ '(default ((t (:background "color-232" :foreground "color-253"))))
+ '(font-lock-builtin-face ((t (:foreground "brightyellow"))))
+ '(font-lock-comment-face ((t (:foreground "color-241"))))
+ '(font-lock-constant-face ((t (:foreground "brightmagenta"))))
+ '(font-lock-negation-char-face ((t (:foreground "brightmagenta"))))
+ '(font-lock-function-name-face ((t (:foreground "brightgreen"))))
+ '(font-lock-keyword-face ((t (:foreground "brightred"))))
+ '(font-lock-string-face ((t (:foreground "cyan"))))
+ '(font-lock-type-face ((t (:foreground "brightcyan"))))
+ '(font-lock-variable-name-face ((t (:foreground "brightgreen"))))
+ '(region ((t (:background "#5F005F"))))
+ '(hl-line ((t (:background "color-235"))))
+ )
