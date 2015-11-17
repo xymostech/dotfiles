d8d961aaeea092db2d0e962d383decd841b146a7
Emily Eisenberg <xymostech@gmail.com>
2015-11-16 18:21:58 -0800

    Make programs better

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,24 +1,33 @@
 #!/usr/bin/env bash
 
-mount | grep -q "/media/Backup"
-if test $?
+OUTPUT=$(date "+%d-%m-%Y-backup.log")
+echo "Writing to $OUTPUT"
+
+if mount | grep -q "/media/Backup"
 then
+    sudo rsync \
+         --partial \
+         --progress \
+         --archive \
+         --verbose \
+         --delete \
+        --exclude="/tmp/*" \
+        --exclude="/sys/*" \
+        --exclude="/proc/*" \
+        --exclude="/dev/*" \
+        --exclude="/run/*" \
+        --exclude="/boot/*" \
+        --exclude="/media/*" \
+        --exclude="/mnt/*" \
+        --exclude="/swapfile*" \
+        --exclude="/home/xymostech/Downloads/*" \
+        --exclude="/home/xymostech/Dropbox/*" \
+        --exclude="/home/xymostech/.steam/*" \
+        --exclude="/home/xymostech/.local/share/Steam/*" \
+        --exclude="/home/xymostech/khan/webapp/.git/modules/*" \
+        / \
+        /media/Backup > "$OUTPUT"
+else
     echo "Backup drive isn't mounted"
     exit 1
 fi
-
-sudo rsync --partial --progress --archive --verbose --delete \
-    --exclude="/tmp/*" \
-    --exclude="/sys/*" \
-    --exclude="/proc/*" \
-    --exclude="/dev/*" \
-    --exclude="/run/*" \
-    --exclude="/boot/*" \
-    --exclude="/media/*" \
-    --exclude="/mnt/*" \
-    --exclude="/swapfile" \
-    --exclude="/home/xymostech/Downloads/*" \
-    --exclude="/home/xymostech/Dropbox/*" \
-    --exclude="/home/xymostech/.steam/*" \
-    --exclude="/home/xymostech/.local/share/Steam/*" \
-    / /media/Backup
